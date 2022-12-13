c53d3966eee87645ba46314fc94e75efad3a3837
hansun <hsun7920@bths.edu>
2022-12-12 21:25:24 -0500

    I am ashamed, for I had only seen new heights after standing on the shoulder of a giant.

--- src/LogMessage.java
+++ src/LogMessage.java
@@ -2,6 +2,17 @@ public class LogMessage {
     private String machineId;
     private String description;
 
-    public LogMessage(String message)
+    public LogMessage(String message){
+        machineId = message.substring(0, message.indexOf(":"));
+        description = message.substring(message.indexOf(":") + 1);
+    }
+
+    public boolean containsWord(String word){
+        String newDescription = " " + description + " ";
+        String newWord = " " + word + " ";
+        if(newDescription.contains(newWord)){
+            return true;
+        } return false;
+    }
 
 }
